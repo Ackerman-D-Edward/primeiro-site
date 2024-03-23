ae12bb8d7eae9d46f55d36cc00f0177b3f43f79a
Ackerman-D-Edward <163280780+Ackerman-D-Edward@users.noreply.github.com>
2024-03-23 22:45:04 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,  
+    adaptiveHeight: true,
+});
